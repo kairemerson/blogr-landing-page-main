b65bcb7e222f79ccafb43b949c35812b0a460b83
kairemerson <kairemerson@gmail.com>
2021-06-07 23:01:25 -0300

    coded section future

--- js/script.js
+++ js/script.js
@@ -5,5 +5,6 @@ const links = document.querySelectorAll(".menu-desktop li")
 links.forEach((link)=>{
     link.addEventListener("click", ()=>{
         link.querySelector("div").classList.toggle("active")
+        link.classList.toggle("activea")
     })
 })
