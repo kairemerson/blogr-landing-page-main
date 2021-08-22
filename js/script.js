db074b101b0bfd03882e91d407b3df7d68947734
kairemerson <kairemerson@gmail.com>
2021-08-22 12:29:45 -0300

    finalized the project

--- js/script.js
+++ js/script.js
@@ -10,11 +10,13 @@ links.forEach((link)=>{
 })
 
 
-//button mobile
 let btnMobile = document.querySelector('.btn-mobile')
 let ul = document.querySelector(".menu-desktop")
 let call = document.querySelector("#call-to-action")
+let img = document.querySelector(".future img")
+let imgLaptop = document.querySelector(".powerful img")
 
+//button mobile hamburger and close
 btnMobile.addEventListener("click", function(e){
     if(btnMobile.getAttribute("src") == "./images/icon-hamburger.svg"){
         btnMobile.src = "./images/icon-close.svg"
@@ -27,21 +29,32 @@ btnMobile.addEventListener("click", function(e){
     }
 })
 
-//voltar o call to action para display block
+//voltar o call to action para display block e mudar imagens
 window.onresize = function(){
+    loadCall()
     loadImg()
 }
 window.onload = function(){
+    loadCall()
     loadImg()
 }
-function loadImg(){
-    if(screen.availWidth <= 375){
-        call.style.display = "none"
-    }else{
+function loadCall(){
+    if(screen.availWidth > 375){
         call.style.display = "block"
+    }else{
+        call.style.display = "none"
         
     }
 }
 
+function loadImg(){
+    if(screen.availWidth <= 375){
+        img.src = "./images/illustration-editor-mobile.svg"
+        imgLaptop.src = "./images/illustration-laptop-mobile.svg"
+    }else{
+        img.src = "./images/illustration-editor-desktop.svg"
+        imgLaptop.src = "./images/illustration-laptop-desktop.svg"
+    }
+}
 
 
