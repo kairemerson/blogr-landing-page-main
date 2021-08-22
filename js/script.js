212cdbbe81be219c5aefc258982fa64dc044b587
kairemerson <kairemerson@gmail.com>
2021-08-21 21:12:05 -0300

    finalized menu mobile and dropdown mobile

--- js/script.js
+++ js/script.js
@@ -12,12 +12,36 @@ links.forEach((link)=>{
 
 //button mobile
 let btnMobile = document.querySelector('.btn-mobile')
+let ul = document.querySelector(".menu-desktop")
+let call = document.querySelector("#call-to-action")
 
 btnMobile.addEventListener("click", function(e){
     if(btnMobile.getAttribute("src") == "./images/icon-hamburger.svg"){
         btnMobile.src = "./images/icon-close.svg"
+        ul.style.display = "block"
+        call.style.display = "block"
     }else{
         btnMobile.src = "./images/icon-hamburger.svg"
-        
+        ul.style.display = "none"
+        call.style.display = "none"
     }
 })
+
+//voltar o call to action para display block
+window.onresize = function(){
+    loadImg()
+}
+window.onload = function(){
+    loadImg()
+}
+function loadImg(){
+    if(screen.availWidth <= 375){
+        call.style.display = "none"
+    }else{
+        call.style.display = "block"
+        
+    }
+}
+
+
+
